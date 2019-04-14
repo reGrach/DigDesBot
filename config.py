88112fe72921e8e2ae3d3371f78a7fa7ed8b0356
German Grachev <germangrach.94@ya.ru>
2019-04-15 00:49:27 +0300

    Update config, add setup.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Токен для подключения к боту
-TOKEN_BOT = '775089324:AAFSpp_qcG0r_USD406yDzt3W77FPC85oqU'
+TOKEN_BOT = 'TOKEN_BOT'
 
 # Cписок юзеров-админов
 USRES = [
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DigDesBot',
+    version='0.1.0',
+    packages=['pyTelegramBotAPI'],
+    url='https://github.com/reGrach/DigDesBot',
+    license='',
+    author='reGrach',
+    author_email='GermanGrach.94@ya.ru',
+    description='Корпоративный, информационный телеграм-бот, созданный для сотрудников компании Digital Design'
+)
